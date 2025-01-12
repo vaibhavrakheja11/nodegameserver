ed8e31cd6570f0185b1df60455120ce0e1f59146
vaibhavrakheja11 <vaibhavrakheja11@gmail.com>
2025-01-12 05:13:09 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const crypto = require('crypto');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3008;
 const host = 'localhost';  // Change this to your desired host
 
 app.use(cors());
